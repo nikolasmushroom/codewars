ab570fc818d6fcc81e52c43a38045f0410466f3e
nikolasmushroom <dashagribok0905@gmail.com>
2024-07-15 19:35:02 +0300

    double-cha.js - 1;

---
+++ 8kyu/double-cha.js
@@ -0,0 +1,20 @@
+// ❓ DESCRIPTION:
+//     Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+//
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+// ✅ SOLUTION:
+function doubleChar(str) {
+    let res = [];
+    let arr = str.split('');
+    for(let i = 0; i < arr.length; i++){
+        res.push(arr[i])
+        res.push(arr[i])
+    }
+    return res.join('');
+}
+
+console.log(doubleChar('string'));
