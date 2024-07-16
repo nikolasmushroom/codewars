2f647e030a4ea7da87a5137695e3c8d05d54561b
nikolasmushroom <dashagribok0905@gmail.com>
2024-07-16 20:55:45 +0300

    only-duplicates.js - simpler answer with filter;

--- 6kyu/only-duplicates.js
+++ 6kyu/only-duplicates.js
@@ -9,28 +9,7 @@
 
 // ✅ SOLUTIONS:
 function onlyDuplicates(str) {
-    let res = [];
-    let arr = str.split('')
-    let index = 0;
-    for(let i = 0; i < arr.length; i++){
-        for(let x = i; x < arr.length; x++){
-            if(arr[x + 1] === arr[i] && index === 0){
-                res.push(arr[i])
-                res.push(arr[x + 1])
-                index++;
-            }else if(arr[x + 1] === arr[i] && index !== 0){
-                res.push(arr[x + 1])
-                index++;
-            }
-        }
-    }
-    for(let y = 0; y < arr.length; y++){
-        if(!res.includes(arr[y])){
-            arr.splice(arr.indexOf(arr[y]), 1);
-            y--;
-        }
-    }
-    return arr.join('');
+    return str.split('').filter(e => str.indexOf(e) !== str.lastIndexOf(e)).join('');
 }
 
 console.log(onlyDuplicates('colloquial'))
