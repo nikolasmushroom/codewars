267de108e573ef6766b96548f47cf35d5827d7ae
nikolasmushroom <dashagribok0905@gmail.com>
2024-07-16 21:21:54 +0300

    not-very-secure.js - 1;

---
+++ 5kyu/not-very-secure.js
@@ -0,0 +1,25 @@
+// ❓ DESCRIPTION:
+//     In this example you have to validate if a user input string is alphanumeric. The given string is not nil/null/NULL/None, so you don't have to check that.
+//
+// The string has the following conditions to be alphanumeric:
+//
+//     At least one character ("" is not valid)
+// Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
+// No whitespaces / underscore
+
+// ✅ SOLUTION:
+function alphanumeric(string){
+    let arr = string.split('');
+    if(arr.length === 0){
+        return false;
+    }
+    console.log(arr);
+    for(let i = 0; i < arr.length; i++){
+        if(!/^[A-Za-z0-9-]+$/.test(arr[i])){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(alphanumeric(""))
