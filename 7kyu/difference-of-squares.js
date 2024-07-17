9e9613e6ee4947cbb8e7ccdc52013c8dd031f594
nikolasmushroom <dashagribok0905@gmail.com>
2024-07-17 13:33:55 +0300

    difference-of-squares.js - 1;

---
+++ 7kyu/difference-of-squares.js
@@ -0,0 +1,28 @@
+// ❓ DESCRIPTION:
+//     Recreation of Project Euler problem #6
+//
+// Find the difference between the square of the sum of the first n natural numbers (1 <= n <= 100) and the sum of their squares.
+//
+//     Example
+// For example, when n = 10:
+//
+// The square of the sum of the numbers is:
+//
+//     (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)² = 55² = 3025
+// The sum of the squares of the numbers is:
+//
+//     1² + 2² + 3² + 4² + 5² + 6² + 7² + 8² + 9² + 10² = 385
+// Hence the difference between square of the sum of the first ten natural numbers and the sum of the squares of those numbers is: 3025 - 385 = 2640
+
+// ✅ SOLUTION:
+function differenceOfSquares(n) {
+    let sum1 = 0;
+    let sum2 = 0
+    for(let i = 1; i <= n; i++){
+        sum1 += i;
+        sum2 += i ** 2;
+    }
+    return sum1 ** 2 - sum2;
+}
+
+console.log(differenceOfSquares(5))
