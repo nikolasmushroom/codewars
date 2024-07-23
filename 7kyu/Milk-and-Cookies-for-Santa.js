6fabc743d4f673f4bfe020b1a33f5a0a1f463490
nikolasmushroom <dashagribok0905@gmail.com>
2024-07-23 22:48:25 +0300

    Milk-and-Cookies-for-Santa.js - 1;

---
+++ 7kyu/Milk-and-Cookies-for-Santa.js
@@ -0,0 +1,26 @@
+// ❓ DESCRIPTION:
+//     Happy Holidays fellow Code Warriors!
+//     It's almost Christmas Eve, so we need to prepare some milk and cookies for Santa! Wait... when exactly do we need to do that?
+//
+// Time for Milk and Cookies
+// Complete the function function that accepts a Date object, and returns true if it's Christmas Eve (December 24th), false otherwise.
+//
+// Keep in mind Javascript's Date month is 0 based!
+//
+// Examples
+// timeForMilkAndCookies(new Date(2013, 11, 24))  // true
+// timeForMilkAndCookies(new Date(2013, 0, 23))   // false
+// timeForMilkAndCookies(new Date(3000, 11, 24))  // true
+
+// ✅ SOLUTION:
+function timeForMilkAndCookies(date){
+    let day = date.getDay();
+    let month = date.getMonth();
+    if(day === 24 && month === 11){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(timeForMilkAndCookies(timeForMilkAndCookies(new Date(2013, 11, 24)) ));
