8389a38ab13c0d7b3d2ec4895426657c77951d10
nikolasmushroom <dashagribok0905@gmail.com>
2024-07-26 17:23:47 +0300

    Coding-Meetup-1-Higher-Order-FunctionsSeries-Count-the-number-of-JavaScript-developers-coming-from-Europe.js - 2;

--- 7kyu/Coding-Meetup-1-Higher-Order-FunctionsSeries-Count-the-number-of-JavaScript-developers-coming-from-Europe.js
+++ 7kyu/Coding-Meetup-1-Higher-Order-FunctionsSeries-Count-the-number-of-JavaScript-developers-coming-from-Europe.js
@@ -32,7 +32,7 @@ function countDevelopers(list) {
 }
 
 console.log(countDevelopers([
-    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+    { firstName: 'Noah', lastName: 'M.', country: 'USA', continent: 'America', age: 19, language: 'JavaScript' },
     { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
     { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
     { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
