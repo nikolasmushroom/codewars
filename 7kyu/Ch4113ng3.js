78394749aea91ae6e00655343543b259d39f102b
nikolasmushroom <dashagribok0905@gmail.com>
2024-07-29 23:50:13 +0300

    Ch4113ng3.js - 1;

---
+++ 7kyu/Ch4113ng3.js
@@ -0,0 +1,22 @@
+// ❓ Description:
+//     Make your strings more nerdy: Replace all 'a'/'A' with 4, 'e'/'E' with 3 and 'l' with 1 e.g. "Fundamentals" --> "Fund4m3nt41s"
+
+// ✅ SOLUTION:
+function nerdify(txt){
+    let arr = txt.split('');
+    let res = [];
+    for(let elem of arr){
+        if(elem === 'a' || elem === 'A'){
+            res.push('4');
+        }else if(elem === 'e' || elem === 'E'){
+            res.push('3');
+        }else if(elem === 'l'){
+            res.push('1');
+        }else{
+            res.push(elem);
+        }
+    }
+    return res.join('')
+}
+
+console.log(nerdify('Philadelphia'));
