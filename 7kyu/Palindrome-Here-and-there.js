fb7147b0cea19a4a82d514b339c9d3fd4d120fec
nikolasmushroom <dashagribok0905@gmail.com>
2024-07-30 22:44:22 +0300

    Palindrome-Here-and-there.js - 1;

---
+++ 7kyu/Palindrome-Here-and-there.js
@@ -0,0 +1,23 @@
+// ❓ DESCRIPTION:
+//     An array is given with palindromic and non-palindromic numbers. A palindromic number is a number that is the same from a reversed order. For example 122 is not a palindromic number, but 202 is one.
+//
+//     Your task is to write a function that returns an array with only 1s and 0s, where all palindromic numbers are replaced with a 1 and all non-palindromic numbers are replaced with a 0.
+//
+// For example:
+//
+//     [101, 2, 85, 33, 14014]  ==>  [1, 1, 0, 1, 0]
+//         [45, 21, 303, 56]        ==>  [0, 0, 1, 0]
+
+// ✅ SOLUTION:
+function convertPalindromes(numbers) {
+    let res = [];
+    numbers.forEach((elem) => {
+        if(String(elem).split('').reverse().join('') === String(elem)){
+            res.push(1);
+        }else{
+            res.push(0);
+        }
+    })
+    return res;
+}
+console.log(convertPalindromes([11, 22, 63]));
