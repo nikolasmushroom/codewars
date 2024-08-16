86a9719251ef98a03c82092578e741da8800a09a
nikolasmushroom <dashagribok0905@gmail.com>
2024-08-16 19:57:56 +0300

    A===B.js - 1;

---
+++ 7kyu/A===B.js
@@ -0,0 +1,26 @@
+// ❓ Description:
+//     Compare 2 integers and return if they are equals, don't use the comparison Operators
+//
+// Can't use these Operators
+//
+// Operator	Description
+// ==	equal to
+// ===	equal value and equal type
+// !=	not equal
+// !==	not equal value or not equal type
+// >	greater than
+// <	less than
+// >=	greater than or equal to
+// <=	less than or equal to
+// Example:
+//     0 == 0  => true
+// 0 == 1  => false
+
+// ✅ Solution:
+function d01(a,b) {
+    let arr1 = [a];
+    let arr2 = [b];
+    return arr1.includes(arr2[0]) ? true : false;
+}
+
+console.log(d01(1, 2))
