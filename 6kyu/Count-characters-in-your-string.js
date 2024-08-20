d4459bd88e1c83f70c787ab9d5b0c28cb94bf81e
nikolasmushroom <dashagribok0905@gmail.com>
2024-08-20 23:54:59 +0300

    Count-characters-in-your-string.js - 3;

--- 6kyu/Count-characters-in-your-string.js
+++ 6kyu/Count-characters-in-your-string.js
@@ -7,11 +7,12 @@
 function count(string) {
     let obj = {};
     for(let i = 0; i < string.length; i++){
-        if(!obj[string[i]]){
-            obj[string[i]] = 1;
-        }else{
-            obj[string[i]]++;
-        }
+        !obj[string[i]] ? obj[string[i]] = 1 : obj[string[i]]++;
+        // if(!obj[string[i]]){
+        //     obj[string[i]] = 1;
+        // }else{
+        //     obj[string[i]]++;
+        // }
     }
     return obj;
 }
